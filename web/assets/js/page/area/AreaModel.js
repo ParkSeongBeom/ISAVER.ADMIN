649d396d1e1037c9272ee54241b7531a486931d6
dhj <dhj@901543cf-0cf0-41ec-aae1-57714710c31b>
2016-07-22 05:04:29 +0000

    git-svn-id: http://172.16.120.202/svn/ICENT_ISAVER/ISAVER.ADMIN/trunk@235 901543cf-0cf0-41ec-aae1-57714710c31b

--- web/assets/js/page/area/AreaModel.js
+++ web/assets/js/page/area/AreaModel.js
@@ -553,10 +553,3 @@ function AreaModel() {
 
     return AreaModel;
 }
-
-
-Array.prototype.sortByProp = function(p){
-    return this.sort(function(a,b){
-        return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
-    });
-}
--- web/assets/js/page/device/DeviceModel.js
+++ web/assets/js/page/device/DeviceModel.js
@@ -594,10 +594,3 @@ function DeviceModel() {
 
     return DeviceModel;
 }
-
-
-Array.prototype.sortByProp = function(p){
-    return this.sort(function(a,b){
-        return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
-    });
-}
