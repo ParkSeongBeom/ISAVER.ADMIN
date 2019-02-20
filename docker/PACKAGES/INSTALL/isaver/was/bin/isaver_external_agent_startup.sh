4eff617e794d5332ed6e4df303ba5ecb806790dc
ljh <ljh@icent.co.kr>
2019-02-20 12:18:19 +0900

    배포용 파일 임시 제거.

--- docker/PACKAGES/INSTALL/isaver/was/bin/isaver_external_agent_startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup java -server -Xms256m -Xmx512m -XX:NewRatio=12 -XX:PermSize=256m -XX:MaxPermSize=256m -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/isaver/was/isaver_external/heapdump -Dfile.encoding=UTF-8 -jar /isaver/was/isaver_external/ISAVER.EXTERNAL.AGENT.jar /isaver/was/isaver_external/deploy.properties 1> /dev/null 2>&1&
