89ab02539b1116836ba1882b845b5fa446b94ca0
meous <meous@icent.co.kr>
2019-05-16 16:44:52 +0900

    - CSS update

--- src/main/webapp/assets/js/page/dashboard/custom-map-mediator.js
+++ src/main/webapp/assets/js/page/dashboard/custom-map-mediator.js
@@ -593,10 +593,10 @@ var CustomMapMediator = (
                         'targetId' : data['targetId']
                         ,'mainFlag' : data['mainFlag']
                         ,'deviceCode' : data['deviceCode']
-                        ,'x1' : data['useYn']?data['x1']:targetElement.position()['left']+(_mapCanvas.width()/2)
-                        ,'x2' : data['useYn']?data['x2']:_mapCanvas.width()-targetElement.position()['left']-(_mapCanvas.width()/2)-(targetElement.width()/2)
-                        ,'y1' : data['useYn']?data['y1']:targetElement.position()['top']+(_mapCanvas.height()/2)
-                        ,'y2' : data['useYn']?data['y2']:_mapCanvas.height()-targetElement.position()['top']-(_mapCanvas.height()/2)-(targetElement.height()/2)
+                        ,'x1' : data['useYn']?data['x1']:targetElement.position()['left']
+                        ,'x2' : data['useYn']?data['x2']:_mapCanvas.width()-targetElement.position()['left']-(targetElement.width()/2)
+                        ,'y1' : data['useYn']?data['y1']:targetElement.position()['top']
+                        ,'y2' : data['useYn']?data['y2']:_mapCanvas.height()-targetElement.position()['top']-(targetElement.height()/2)
                         ,'useYn' : data['useYn']
                     }
                     ,'element' : targetElement
